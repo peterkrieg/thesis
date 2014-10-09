40a8a3537395565934a6856eb9498537d110372f
Peter Krieg <copperkrieg@gmail.com>
2014-10-09 13:29:44 -0400

    added multiple balls bouncing

--- js/ballbounce/ballbounce2.js
+++ js/ballbounce/ballbounce2.js
@@ -1,8 +1,11 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d'); 
 
+canvas.height = screen.height-200;
+canvas.width = screen.width -100;
+
 var radius = 20;
-var color = "#0000ff";
+var color = "darkgreen";
 var g = 0.1; // acceleration due to gravity
 var x = 50;  // initial horizontal position
 var y = 50;  // initial vertical position
@@ -12,11 +15,18 @@ var vy = 0;  // initial vertical speed
 window.onload = init; 
  
 function init() {
-  setInterval(onEachStep, 1000/60); // 60 fps
+  setInterval(onEachStep, 1000/100); // 60 fps
 };
  
 function onEachStep() {
   vy += g; // gravity increases the vertical speed
+
+  if (vx >0){   // while vx is still positive, decrease it incrementally to represent air resistance/friction
+    vx -= .001;
+  } else{
+    vx === 0;   // the instant vx is 0 or negative, it is set to 0 to stop the movement in x direction
+  }
+
   x += vx; // horizontal speed increases horizontal position 
   y += vy; // vertical speed increases vertical position
  
