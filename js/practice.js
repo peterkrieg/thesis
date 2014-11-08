b93f9da1e9e7c2e91a565c2bc9b542fbe4a90784
Peter Krieg <copperkrieg@gmail.com>
2014-11-07 19:42:39 -0500

    practiced drawing on HTML5 canvas, and created folder for actual written version of thesis

--- js/practice.js
+++ js/practice.js
@@ -10,3 +10,25 @@ sayHello();
 
 console.log(sayHello.name);
 
+
+//   here I will start the part involving the canvas
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+context.strokeStyle = '#0000ff'; 
+context.lineWidth = 10; 
+context.beginPath() ; 
+context.moveTo(20, 100); 
+context.lineTo(250, 400); 
+context.stroke();
+
+context.beginPath();
+context.moveTo(250, 400);
+context.lineTo(500, 500);
+context.stroke();
+
+context.strokeStyle= '#ff0000';
+context.fillStyle = 'red';
+context.fillRect(20, 40, 10, 50);
+
