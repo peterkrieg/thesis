991ff818a657c8edd977831644baaa7e23ea11b3
Peter Krieg <copperkrieg@gmail.com>
2014-11-16 10:23:12 -0500

    edited basic ball bounce code to look better

--- js/ballbounce/ballbounce.js
+++ js/ballbounce/ballbounce.js
@@ -6,10 +6,10 @@ canvas.width = screen.width -100;
 
 var radius = 20;
 var color = "red";
-var g = .20; // acceleration due to gravity
+var g = .1635; // acceleration due to gravity
 var x = 40;  // initial horizontal position
 var y = 40;  // initial vertical position
-var vx = prompt('what is the initial horizontal speed of ball you would like?  (recommended values of 1-20');  // initial horizontal speed 
+var vx = parseInt(prompt('what is the initial horizontal speed of ball you would like?(recommended values of 1-20'));  // initial horizontal speed 
 var vy = 0;  // initial vertical speed
  
 window.onload = init; 
@@ -20,17 +20,15 @@ function init() {
  
 function onEachStep() {
   vy += g; // gravity increases the vertical speed
-
-  if(y=== canvas.height - radius){
-    vx *= -.1
-  }
-
   x += vx; // horizontal speed increases horizontal position 
   y += vy; // vertical speed increases vertical position
- 
+
+  if(y=== canvas.height - radius){
+    vx *= .999 // friction between floor and ball
+  }
   if (y > canvas.height - radius){ // if ball hits the ground
     y = canvas.height - radius; // reposition it at the ground
-    vy *= -0.9; // then reverse and reduce its vertical speed
+    vy *= -0.7; // then reverse and reduce its vertical speed
   }
   if (x > canvas.width - radius){ // if ball hits right wall
     x = canvas.width - radius; // reposition it right at wall 
@@ -39,7 +37,6 @@ function onEachStep() {
   if (x < radius){  // if ball hits left wall
     x = radius;  // reposition it right at wall
     vx *= -0.8  // then reverse and reduce horizontal speed
-
   }
   drawBall(); // draw the ball
 };
