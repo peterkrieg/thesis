e1f1974d01119808c7183f77562d6e0355650a71
Peter Krieg <copperkrieg@gmail.com>
2014-11-23 00:12:02 -0500

    experimented more with canvas

--- js/practice.js
+++ js/practice.js
@@ -1,17 +1,13 @@
 
+// function sayHello(){
+// 	console.log('hello world again');
+// 	this.name = "something";
+// 	this.age = 22;
+// }
 
+// sayHello();
 
-console.log('hello world!!!');
-
-function sayHello(){
-	console.log('hello world again');
-	this.name = "something";
-	this.age = 22;
-}
-
-sayHello();
-
-console.log(sayHello.name);
+// console.log(sayHello.name);
 
 
 //   here I will start the part involving the canvas
@@ -23,24 +19,21 @@ canvas.width=screen.width;
 
 context.strokeStyle = '#0000ff'; 
 context.lineWidth = 5; 
-context.beginPath() ; 
-context.moveTo(20, 100); 
-context.lineTo(250, 400); 
-context.stroke();
 
-context.moveTo(250, 400);
-context.lineTo(500, 500);
-context.stroke();
+// context.beginPath() ; 
+// context.moveTo(20, 100); 
+// context.lineTo(250, 400); 
+// context.stroke();
 
-context.fillRect(20, 40, 1370, 50);
-
-
-context.moveTo(300, 300);
-context.lineTo(300, 600);
-context.stroke();
+// context.moveTo(250, 400);
+// context.lineTo(500, 500);
+// context.stroke();
 
 
-context.clearRect(0, 0, canvas.width, canvas.height);
+
+// context.moveTo(300, 300);
+// context.lineTo(300, 600);
+// context.stroke();
 
 
 
@@ -48,35 +41,32 @@ context.clearRect(0, 0, canvas.width, canvas.height);
 
 
 
-context.moveTo(0, 0);
+
+
 var x = 0;
-var y = 0;
+var y = 500;
 
 function init(){
-	setInterval(onEachStep, 1000/60);
+	setInterval(onEachStep, 500);
 }
 
 
+context.moveTo(0, 500);
 
 function onEachStep(){
 	context.lineTo(x, y);
 	context.stroke()
-	x+=2;
-	if(y<100){
-		y+= .0003*(x*x);
-	}
-	else if (y<200){
-		y+= 1;
- 	}
- 	else if (y<600){
- 		y+= 2.5;
- 	}
+	x+=72;
+	
  
 	
 
 }
 
 init();
+
+
+console.log('screen widht is  ' + screen.width);
 
 
 
