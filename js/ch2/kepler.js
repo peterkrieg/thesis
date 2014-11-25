91b03a703acda91af017f2982b4776d666e8cbcd
Peter Krieg <copperkrieg@gmail.com>
2014-11-25 01:16:58 -0500

    added kepler html and js for the 3rd and last simulation of chapter 3

--- js/ch2/kepler.js
+++ js/ch2/kepler.js
@@ -9,21 +9,18 @@ var m = 1; // planet's mass
 var M = 1000000; // sun's mass
 var G = 1;
 var t0,dt;
-var i = 0;
-
 
 window.onload = init; 
 
 function init() {
 	// create a stationary sun
-	sun = new Ball(400,'orange',M);
-	sun.pos2D = new Vector2D(500,2900);	
+	sun = new Ball(70,'orange',M);
+	sun.pos2D = new Vector2D(475,400);	
 	sun.draw(context_bg);
 	// create a moving planet			
-	planet = new Ball(10,'blue',m);
-	planet.pos2D = new Vector2D(500,2490);
-	planet.velo2D = new Vector2D(0, -66);
-	console.log(planet.velo2D.length())
+	planet = new Ball(5,'blue',m);
+	planet.pos2D = new Vector2D(375,350);
+	planet.velo2D = new Vector2D(100, -100);
 	// planet.velo2D = new Vector2D(70,-40);
 	// planet.velo2D = new Vector2D(85,-40);
 	// planet.velo2D = new Vector2D(80,0);
@@ -51,13 +48,11 @@ function move(){
 	calcForce();
 	updateAccel();
 	updateVelo(planet);
-	i++;
-	if(i%15 ===0){console.log(planet.velo2D.length());}
 }
 
 function moveObject(obj){
 	obj.pos2D = obj.pos2D.addScaled(obj.velo2D,dt);	
-	context.clearRect(0, 0, canvas.width, canvas.height);
+	// context.clearRect(0, 0, canvas.width, canvas.height);
 	obj.draw(context);	
 }
 function calcForce(){
